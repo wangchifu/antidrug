e7d5fc3f1d51dc1af66b78db739030aa8787719c
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-03-27 14:11:34 +0800

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,8 +9,7 @@ $setup = \App\Models\Setup::first();
     <meta name="description" content="Ogani Template">
     <meta name="keywords" content="Ogani, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src 'self' data:;">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title>{{ $setup->website_name }}</title>
 
     <!-- Google Font -->
